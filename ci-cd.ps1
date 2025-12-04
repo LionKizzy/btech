73b2ebeaa451788073664e13b871b9e34255f5f7
lion <ndatabayeleoncegmail.com>
2025-12-04 15:06:52 +0200

    fix aut

--- ci-cd.ps1
+++ ci-cd.ps1
@@ -6,7 +6,7 @@
 $ErrorActionPreference = "Stop"
 
 # -----------------------------------
-# 1. Ensure Minikube is Running  12346
+# 1. Ensure Minikube is Running  123467
 # -----------------------------------
 
 Write-Host "Checking Minikube status..." -ForegroundColor Cyan
